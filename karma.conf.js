deafed6972117ec3fb6e63c240e1b60daba22ecc
Italo Rocha <italorochaj@icloud.com>
2026-02-20 02:06:03 -0300

    chore: atualizar configurações de build e dependências
    - Atualizado karma.conf.js para cobertura de código aprimorada
    - Atualizado package.json com script test:ci para execução sem watch
    - Configurado coverage reporter com thresholds

--- karma.conf.js
+++ karma.conf.js
@@ -1,6 +1,3 @@
-// Karma configuration file, see link for more information
-// https://karma-runner.github.io/1.0/config/configuration-file.html
-
 module.exports = function (config) {
   config.set({
     basePath: '',
@@ -8,25 +5,68 @@ module.exports = function (config) {
     plugins: [
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
-      require('karma-jasmine-html-reporter'),
       require('karma-coverage-istanbul-reporter'),
       require('@angular-devkit/build-angular/plugins/karma')
     ],
     client: {
-      clearContext: false // leave Jasmine Spec Runner output visible in browser
+      clearContext: false,
+      jasmine: {
+        random: false
+      }
     },
     coverageIstanbulReporter: {
       dir: require('path').join(__dirname, './coverage/start-dev'),
-      reports: ['html', 'lcovonly', 'text-summary'],
-      fixWebpackSourcePaths: true
+      reports: ['html', 'lcovonly', 'text-summary', 'text'],
+      fixWebpackSourcePaths: true,
+      emitWarning: true,
+      skipFilesWithNoCoverage: false,
+      thresholds: {
+        statements: 0,
+        lines: 0,
+        branches: 0,
+        functions: 0
+      }
     },
-    reporters: ['progress', 'kjhtml'],
+    reporters: ['progress', 'coverage-istanbul'],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
     browsers: ['Chrome'],
     singleRun: false,
-    restartOnFileChange: true
+    restartOnFileChange: true,
+    browserNoActivityTimeout: 180000,
+    browserDisconnectTimeout: 30000,
+    browserDisconnectTolerance: 5,
+    captureTimeout: 180000,
+    reportSlowerThan: 500,
+    tmpDir: './karma-tmp',
+    customLaunchers: {
+      ChromeHeadlessCI: {
+        base: 'ChromeHeadless',
+        flags: [
+          '--no-sandbox',
+          '--disable-gpu',
+          '--disable-dev-shm-usage',
+          '--disable-software-rasterizer',
+          '--disable-extensions',
+          '--disable-web-security',
+          '--disable-background-networking',
+          '--disable-client-side-phishing-detection',
+          '--disable-popup-blocking',
+          '--disable-prompt-on-repost',
+          '--disable-sync',
+          '--metrics-recording-only',
+          '--mute-audio',
+          '--no-default-browser-check',
+          '--no-first-run',
+          '--safebrowsing-disable-auto-update',
+          '--enable-automation',
+          '--enable-features=NetworkService,NetworkServiceInProcess',
+          '--allow-running-insecure-content',
+          '--disable-site-isolation-trials'
+        ]
+      }
+    }
   });
 };
